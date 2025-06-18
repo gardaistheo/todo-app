a8d23370b9527118644eb01e3e3a7aa1e32df6ae
c82hgar <gardais.theo@imsa.msa.fr>
2025-06-18 15:19:56 +0200

    adding test

---
+++ frontend/src/App.test.js
@@ -0,0 +1,13 @@
+// frontend/src/App.test.js
+import { render, screen} from '@testing-library/react';
+import App from './App';
+
+beforeEach(() => {
+  fetch.resetMocks();
+});
+
+test('affiche le titre de la page', async () => {
+  fetch.mockResponseOnce(JSON.stringify([]));
+  render(<App />);
+  expect(screen.getByText('Ma Todo List')).toBeInTheDocument();
+});
