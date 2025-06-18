9e8cb751c5f7b5931bd17338237712582036800a
Merlin Barbou <merlin31770@gmail.com>
2025-06-18 16:14:07 +0200

    integration eslint

---
+++ frontend/.eslintrc.js
@@ -0,0 +1,27 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+    jest: true,
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:react/recommended',
+  ],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  plugins: [
+    'react',
+  ],
+  rules: {
+    // Ajoute tes règles personnalisées ici
+  },
+  settings: {
+    react: {
+      version: 'detect',
+    },
+  },
+};
